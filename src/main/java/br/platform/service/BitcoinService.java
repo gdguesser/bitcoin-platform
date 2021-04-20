70c7671ffa87c97e0e089a7dd379a1715711475d
Gabriel Guesser <gabrieldguesser@hotmail.com>
2021-04-20 18:38:04 -0300

    configuring application properties

--- src/main/java/br/platform/service/BitcoinService.java
+++ src/main/java/br/platform/service/BitcoinService.java
@@ -1,11 +1,15 @@
 package br.platform.service;
 
 import br.platform.model.Bitcoin;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
+@Path("/bitcoins")
+@RegisterRestClient
 public interface BitcoinService {
 
     @GET
