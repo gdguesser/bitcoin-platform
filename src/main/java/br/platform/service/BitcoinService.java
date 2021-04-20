bc0dee5c004eb3b50dc256ae13c72627fbe52f55
Gabriel Guesser <gabrieldguesser@hotmail.com>
2021-04-20 18:27:20 -0300

    creating listAll method signature

--- src/main/java/br/platform/service/BitcoinService.java
+++ src/main/java/br/platform/service/BitcoinService.java
@@ -1,4 +1,14 @@
 package br.platform.service;
 
+import br.platform.model.Bitcoin;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
 public interface BitcoinService {
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Bitcoin> listAll();
 }
