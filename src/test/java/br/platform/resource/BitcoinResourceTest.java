9f52f8fafc7a6a210e501411fd9e2c8d49a5b890
Gabriel Guesser <gabrieldguesser@hotmail.com>
2021-04-20 20:50:10 -0300

    fixing restassured dependency and implementing test for the request

--- src/test/java/br/platform/resource/BitcoinResourceTest.java
+++ src/test/java/br/platform/resource/BitcoinResourceTest.java
@@ -2,12 +2,14 @@ package br.platform.resource;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import io.restassured.*;
 
 @QuarkusTest
 public class BitcoinResourceTest {
 
     @Test
     public void testIfStatusCodeOfRequestIs200() {
-        RestAssured.given().get("")
+        RestAssured
+                .given().get("bitcoins").then().statusCode(200);
     }
 }
