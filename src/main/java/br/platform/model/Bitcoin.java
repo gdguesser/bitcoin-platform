bf03fd1e815f62495e7c100b34c5d2dd3788d98e
Gabriel Guesser <gabrieldguesser@hotmail.com>
2021-04-20 18:24:00 -0300

    creating bitcoin class and bitcoin service interface

---
+++ src/main/java/br/platform/model/Bitcoin.java
@@ -0,0 +1,13 @@
+package br.platform.model;
+
+import java.time.LocalDate;
+
+public class Bitcoin {
+
+    private Long id;
+    private Double price;
+    private String type;
+    private LocalDate date;
+
+
+}
---
+++ src/main/java/br/platform/service/BitcoinService.java
@@ -0,0 +1,4 @@
+package br.platform.service;
+
+public interface BitcoinService {
+}
