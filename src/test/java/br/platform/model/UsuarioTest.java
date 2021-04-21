ba4cd7ad114bf8ebcfb4057c5338dab27adf1720
Gabriel Guesser <gabrieldguesser@hotmail.com>
2021-04-21 11:50:33 -0300

    creating test for the user model with activerecords

---
+++ src/test/java/br/platform/model/UsuarioTest.java
@@ -0,0 +1,26 @@
+package br.platform.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.panache.mock.PanacheMock;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+@QuarkusTest
+public class UsuarioTest {
+
+    @Test
+    public void testIfFindByIdOptionalReturnsCorrectUser() {
+        PanacheMock.mock(Usuario.class);
+
+        Usuario u = new Usuario();
+        Optional<PanacheEntityBase> usuario = Optional.of(u);
+
+        Mockito.when(Usuario.findByIdOptional(10)).thenReturn(usuario);
+
+        Assertions.assertSame(u, Usuario.findByIdOptional(10).get());
+    }
+}
