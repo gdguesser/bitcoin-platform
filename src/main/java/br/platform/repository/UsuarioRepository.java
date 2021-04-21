73f1531ca2cbf4d8a9687c2aa10c4505c25ae003
Gabriel Guesser <gabrieldguesser@hotmail.com>
2021-04-20 23:25:15 -0300

    annotating user repository with @ApplicationScoped

--- src/main/java/br/platform/repository/UsuarioRepository.java
+++ src/main/java/br/platform/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package br.platform.repository;
 
 import br.platform.model.Usuario;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import javax.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class UsuarioRepository implements PanacheRepository<Usuario> {
 }
