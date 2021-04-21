ef1321110fb0b84a7a8f0fa84d8a78118bf48bda
Gabriel Guesser <gabrieldguesser@hotmail.com>
2021-04-21 12:02:42 -0300

    creating test for the order repository with repository type

---
+++ src/test/java/br/platform/repository/OrdemRepositoryTest.java
@@ -0,0 +1,31 @@
+package br.platform.repository;
+
+import br.platform.model.Ordem;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@QuarkusTest
+public class OrdemRepositoryTest {
+
+    @InjectMock
+    OrdemRepository ordemRepository;
+
+    public void testIfListAllReturnsCorrectOrders() {
+
+        Ordem firstOrder = new Ordem();
+        Ordem secondOrder = new Ordem();
+        List<Ordem> orders = new ArrayList<Ordem>();
+
+        orders.add(firstOrder);
+        orders.add(secondOrder);
+
+        Mockito.when(ordemRepository.listAll()).thenReturn(orders);
+
+        Assertions.assertSame(secondOrder, ordemRepository.listAll().get(1));
+    }
+}
